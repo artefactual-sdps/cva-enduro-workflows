b85568aa6d2a90739fc1319799c7b02e03a1105f
David Juhasz <djjuhasz@gmail.com>
2026-02-27 10:31:18 -0800

    Defer context cancel() in worker
    Address gosec warning about not cancelling the worker context if the
    main function exits unexpectedly (i.e. not via an interupt signal).

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -66,6 +66,7 @@ func main() {
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() { <-c; cancel() }()
